e65f421236ac213bdec3f87db44d73a9c52fc676
ethandrzb <ethandrzb@gmail.com>
2021-10-24 18:31:54 -0400

    Added pseudocode for unit tests for ApplicationStateSerializer

---
+++ GraphicalTodoList/src/test/java/logic/ApplicationStateSerializerTest.java
@@ -0,0 +1,27 @@
+package logic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationStateSerializerTest
+{
+
+    @Test
+    void saveListsToFile()
+    {
+        // Create list of TodoLists to use as test data
+
+        // Save TodoLists to a file
+
+        // Check file's contents line by line to make sure they match the expected contents
+    }
+
+    @Test
+    void loadListsFromFile()
+    {
+        // Load TodoLists from file
+
+        // Assert that all items in all TodoLists from file are accessible
+    }
+}
